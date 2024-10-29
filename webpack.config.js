afb3617442080a38e2ab01a281fdcc95b1b02281
EbolaBelle <luke.zimmerman@outlook.com>
2024-10-29 10:26:16 -0400

    Add build/dev/deploy scripts to webpack.config.json; clean up repo

--- webpack.config.js
+++ webpack.config.js
@@ -34,4 +34,9 @@ module.exports = {
             },
         ],
     },
+    "scripts": {
+        "build": "webpack",
+        "dev": "webpack serve",
+        "deploy": "git subtree push --prefix dist origin gh-pages"
+    },
   };
